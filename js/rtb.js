cea7834ab9ac5680d0cd2523af08b174a5509fc5
Ryan Brennan <ryan.brennan@dealer.com>
2017-08-16 11:17:31 -0700

    opal message

--- js/rtb.js
+++ js/rtb.js
@@ -9,3 +9,27 @@ $(document).ready(function(){
 	  $("#menu").toggleClass("visible");
 	});
 });
+
+
+
+// opal custom message
+    if (window.location.href.indexOf("opal") > -1) {
+    	$(function() {
+
+    var quotes = $(".opal p");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(1000)
+            .delay(1000)
+            .fadeOut(1000, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
+
+        // do nothing
+    }
